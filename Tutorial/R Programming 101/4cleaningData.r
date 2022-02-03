85cd2523d103952f78ee2ef8a2ca224c86302a3b
oucar <hello@ucaronur.com>
2022-02-03 17:03:34 -0500

    cleaning the data in r programming language

--- Tutorial/R Programming 101/4cleaningData.r
+++ Tutorial/R Programming 101/4cleaningData.r
@@ -3,3 +3,39 @@
 # Find and deal with missing data
 # Find and deal with duplicates
 # Recording values, dealign with the variable types you need etc
+library(tidyverse)
+view(starwars)
+
+
+# Variable Types
+glimpse(starwars)
+class(starwars$gender)
+unique(starwars$gender)
+
+# Factor -> categorical variable (order matters)
+starwars$gender <- as.factor(starwars$gender)
+class(starwars$gender)
+glimpse(starwars)
+
+# feminine comes first, but we can change it
+levels(starwars$gender)
+starwars$gender <- factor((starwars$gender),
+                          levels = c("masculine", "feminine"))
+
+# Shift + CTRL + M to %>% 
+starwars %>% 
+  select(name, height, ends_with("color")) %>% 
+  filter(hair_color %in% c("blond", "brown") & (height < 180)) 
+
+# Dealing with the missing data
+# R programming language doesn't know what to do with the missing data
+mean(starwars$height, na.rm = T)
+
+# Best way to deal with the missing data!!
+# try not to use na.omit()!
+# Only omits if height is na.
+starwars %>% 
+  select(name, gender, hair_color, height) %>% 
+  filter(!complete.cases(.)) %>% 
+  drop_na(height)
+
