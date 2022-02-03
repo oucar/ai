329d1a5ced55535d1ffcce3b2283ea5a0cb3d74e
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-02-03 16:44:24 -0500

    cleaning the data init

--- Tutorial/R Programming 101/1beginning.r
+++ Tutorial/R Programming 101/1beginning.r
@@ -1,6 +1,13 @@
+library("tidyverse")
+
 my_data <- read.csv("friends.csv")
 
 # View Data
-
 head(my_data)
 tail(my_data)
+my_data$p.name
+
+# Start your analysis
+my_data %>% 
+  select(p.name, colleagues) %>%
+  filter(colleagues < 3)
