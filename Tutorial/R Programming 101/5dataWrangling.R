5c84bf5286e7f3e8bb7ef30e6684e31b5415f9fd
oucar <hello@ucaronur.com>
2022-02-03 18:40:48 -0500

    pivot long, wide

--- Tutorial/R Programming 101/5dataWrangling.R
+++ Tutorial/R Programming 101/5dataWrangling.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # View Data
 ?msleep
- glimpse(msleep)
+glimpse(msleep)
 View(msleep) 
 
 # Rename a variable
@@ -23,17 +23,39 @@ unique(msleep$order)
 
 msleep %>% filter((order == "Carnivora" | order == "Primates") & sleep_total > 8) %>% 
   select(name, order, sleep_total) %>% 
-  arrange(-sleep_total) %>% 
-  View()
+  arrange(-sleep_total)
+View(msleep)
 
 # Change observations using mutate
-msleep %>% mutate(brainwt_in_grams = brainwt * 1000) %>% 
-  View()
+msleep %>% mutate(brainwt_in_grams = brainwt * 1000)
+View(mSleep)
 
 # Conditional changes (if_else)
 ## logical vector based on a condition
 size_of_brain <- msleep %>% 
   select(name, brainwt) %>% 
   drop_na(brainwt) %>% 
-  mutate(brain_size = if_else(brainwt > 0.01, "large", "small")) %>% 
-  View()
+  mutate(brain_size = if_else(brainwt > 0.01, "large", "small"))
+View(size_of_brain)
+
+
+##### SUPER IMPORTANT!!! #####
+# Reshape the data from wide to long or long to wide!!
+library(gapminder)
+View(gapminder)
+
+# simplify the dataset
+# multiple year records for the same country, might not be exactly what we want :(
+# This is called long data.
+data <- select(gapminder, country, year, lifeExp)
+View(data)
+
+wide_data <- data %>% 
+  pivot_wider(names_from = "year", values_from = "lifeExp")
+View(wide_data)
+
+# back to normal
+long_data <- wide_data %>% 
+  pivot_longer(2:13, names_to = "year", values_to = "lifeExp")
+View(long_data)
+
