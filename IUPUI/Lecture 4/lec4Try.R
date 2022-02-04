8eec8986cb7ceef04730f32d2a6893b7489a4b08
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-02-03 19:32:58 -0500

    completed the lecture 4 challenges

--- IUPUI/Lecture 4/lec4Try.R
+++ IUPUI/Lecture 4/lec4Try.R
@@ -7,40 +7,39 @@ for(i in 1:nrow(planes)){
   planes$manufacturer2[i]=unlist(strsplit(planes$manufacturer[i]," ")[[1]][1])
 }
 
-# 1 write a function
-name_split<-function(st){
-  return(strsplit(st," ")[[1]][1])
+
+############ 1 ##############
+### WRITE A FUNCTION THAT TAKES A STRING
+### AND SPLITS AT THE SPACES AND RETURNS THE
+### FIRST SUBSTRING (e.g. “Airbus Industry” -> “Airbus”)
+
+name_split <- function(st){
+  st <- str_split(st, " ")[[1]][1]
+  return(st)
 }
+name_split("Merhaba Onur")
+name_split("Airbus Industry")
+name_split("I think this is an edge case but idk.")
 
-# 2 use map_chr to get name from  planes$manufacuturer
-planes$manufacturer2=map_chr(planes$manufacturer,name_split)
 
+############ 2 ##############
+### use map_chr to apply “name_split” to the manufacturer column
+### of the planes data frame
+### MAP: Apply a function to each element of a vector
+planes$manufacturer2 = map_chr(planes$manufacturer,name_split)
 
-flights %>% left_join(planes,by="tailnum") %>%
-        group_by(manufacturer2) %>% summarise(total_distance=sum(distance)) %>%
-        arrange(desc(total_distance))
 
 sum_square<-function(x){
-  sq_sum=0
+  sq_sum = 0
   for(i in 1:length(x)){
-    sq_sum=sq_sum+x[i]*x[i]
+    sq_sum = sq_sum + x[i] * x[i]
   }
   return(sq_sum)
 }
 
-library(Rcpp)
-
-cppFunction("int add(int x, int y, int z){int sum = x + y + z;return sum;}")
-
-flight_new=flights %>% mutate(lag_delay=lag(dep_delay))
-
-
-
-cppFunction("double sumC(NumericVector x) {
-  int n = x.size();
-  double total = 0;
-  for(int i = 0; i < n; ++i) {
-    total += x[i]*x[i];
-  }
-  return total;
-}")
+# 1 + 4 + 9
+sum_square(c(1:3))
+# 1 + 4 + 9 + ...
+sum_square(c(1:15))
+# 1 + 4
+sum_square(c(1:2))
