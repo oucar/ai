39536b6ecca4d668b8348486188e4acb4c2e6c8f
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-02-07 21:31:51 -0500

    completed until l7

--- IUPUI/Lecture 6/lec6.R
+++ IUPUI/Lecture 6/lec6.R
@@ -31,3 +31,74 @@ summary(titanic_model)
 # if answer is negative, use logistics function
 # or you can use type = "response" as variable to get rid of the negative
 predict(titanic_model, data.frame(Sex="male", Age=25, Pclass=2), type="response")
+
+
+
+
+
+#### BOOTSTRAP AND REGRESSION ####
+
+house_model_copy = glm(SalePrice ~ GrLivArea + PoolArea + BedroomAbvGr,
+                  data = house[sample(nrow(house), nrow(house), T),],
+                  family = "gaussian")
+
+# new prediction based on the new sample! 
+prediction = predict(house_model_copy, data.frame(GrLivArea=1500, PoolArea=0,
+                                             FullBath=3, BedroomAbvGr=3))
+
+
+
+
+
+## LECTURE 7
+library(randomForest)
+
+rfmodel=randomForest(house[,c("GrLivArea","PoolArea","FullBath","BedroomAbvGr")],
+                     # TARGET VARIABLE
+                     house$SalePrice,
+                     # how many bootstrap copies
+                     n.trees=1000)
+
+# based on how often these variables show up in splits
+importance(rfmodel)
+
+house$rfpredictions=predict(rfmodel,
+                            house[,c("GrLivArea","PoolArea","FullBath","BedroomAbvGr")])
+
+
+# see how prediction depends on GrLivArea
+partialPlot(rfmodel, 
+            data.frame(house[,c("GrLivArea","PoolArea","FullBath","BedroomAbvGr")]),
+            x.var = "GrLivArea")
+
+### should really cross validate (CV)
+## if we want to compare models
+
+# create random train and test data
+# Really important!
+house_shuffle=house[sample(nrow(house),nrow(house),F),]
+house_train=house[1:1000,]
+house_test=house[1001:1460,]
+
+# train on training data
+rfmodel=randomForest(house_train[,c("GrLivArea","PoolArea","FullBath","BedroomAbvGr")],
+                     house_train$SalePrice,
+                     n.trees=10000,nodesize=20)
+
+glmmodel=glm(SalePrice~GrLivArea+PoolArea+FullBath+BedroomAbvGr,
+             data=house_train,family="gaussian")
+
+#predict on test data
+
+house_test$rfpredictions=predict(rfmodel,
+                                 house_test[,c("GrLivArea","PoolArea","FullBath","BedroomAbvGr")])
+
+house_test$predictions=predict(glmmodel,house_test)
+
+#compare accuracy on held out data
+mean(abs(house_test$rfpredictions-house_test$SalePrice))
+mean(abs(house_test$predictions-house_test$SalePrice))
+
+
+
+
